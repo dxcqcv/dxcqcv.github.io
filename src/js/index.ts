9ff696073bb35bec0c022de43fa2c63d719d03f7
dxcqcv <dxcqcv@gmail.com>
2016-08-10 15:23:51 +0800

    add highlight and fix disqus

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,5 +1,8 @@
 declare var require: any;
-
-require('./ga.js');
+//require('./ga.js');
 require('../css/index.styl');
 require('../css/font-awesome.css');
+
+require('../../lib/highlight/styles/default.css');
+let hljs = require('../../lib/highlight/highlight.pack.js');
+hljs.initHighlightingOnLoad()
--- src/js/pages.ts
+++ src/js/pages.ts
@@ -1,2 +1,7 @@
 let p = require('../css/pages.styl');
 let d = require('../css/index.styl');
+/*
+    <link rel="stylesheet" href="{{ site.baseurl }}/lib/highlight//styles/default.css">
+    <script src="{{ site.baseurl }}/lib/highlight/highlight.pack.js"></script>
+    <script>hljs.initHighlightingOnLoad();</script>    
+    */
